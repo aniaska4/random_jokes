2dae9cfde2eda1b8496168b078956933e168f10e
aniaska4 <aniaska4@wp.pl>
2019-05-10 11:45:48 +0200

    add new function getJokeWordList() and removing the overwritten code

--- app.js
+++ app.js
@@ -22,25 +22,24 @@ const getRandomJokes = () => {
             jokesArea.appendChild(jokeSection);
             jokeSection.innerHTML = `<h3> ${value.value.joke} </h3>`
 
-            //create new letter's section
-            const lettersSection = document.createElement("section");
-            lettersSection.classList.add("letters");
-            jokesArea.appendChild(lettersSection);
+            //create new word section
+            const wordSection = document.createElement("section");
+            wordSection.classList.add("words");
+            jokesArea.appendChild(wordSection);
 
             //create new div with sort words
             const wordText = document.createElement("div");
             wordText.classList.add("wordText");
-            lettersSection.appendChild(wordText);
+            wordSection.appendChild(wordText);
             wordText.classList.add("wordText");
-            lettersSection.appendChild(wordText);
+            wordSection.appendChild(wordText);
 
-            const jokeTextMatch = value.value.joke.match(/[a-z]+/gi);;
-            console.log(jokeTextMatch)
-            
-            const jokeTextSort = jokeTextMatch.sort(byLength);
-            for (let el of jokeTextSort) {
-                    wordText.innerHTML += `<span><li>${el}</li></span>`
-                }
+            const jokeTextMatch = value.value.joke;
+            getJokeWordList(jokeTextMatch)
+                .sort(byLength)
+                .map(e => {
+                    return wordText.innerHTML += `<span><li>${e}</li></span>`
+                });
         } else {
             const jokeText = document.querySelectorAll(".jokesArea");
             jokeText.innerHTML = "We are sorry, but we can not display the content at the moment"
@@ -54,4 +53,8 @@ function byLength(e1, e2) {
     e1 = e1.toString().length;
     e2 = e2.toString().length;
     return e1 > e2 ? 1 : (e1 < e2 ? -1 : 0);
+}
+
+function getJokeWordList(joke) {
+    return joke.match(/[a-z]+/gi);
 }
